0787753b45b8e783260f8734426c380354ae974d
Vinicius Jadiscke <vjtasso@gmail.com>
2021-01-06 15:28:53 -0300

    Update Template

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { request, response } from "express";
 import routes from "./routes/index.routes";
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use(routes);
 
 export default server;
